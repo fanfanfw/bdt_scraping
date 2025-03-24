4a539315cc07e42bcf056a275ce8670a5908155a
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-03-25 05:26:27 +0700

    endpoint menerima inputan id

--- scrap_service/listing_tracker_service_mudahmy/app.py
+++ scrap_service/listing_tracker_service_mudahmy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from scrap_service.listing_tracker_service_mudahmy.listing_tracker_mudahmy import ListingTrackerMudahmy
 
 app = Flask(__name__)
@@ -7,10 +7,12 @@ tracker = ListingTrackerMudahmy()
 @app.route('/track/listings/mudahmy', methods=['POST'])
 def track_listings():
     """
-    Endpoint untuk memeriksa status iklan di database.
+    Endpoint untuk memeriksa status iklan di database berdasarkan ID.
     """
-    tracker.track_listings()
-    return jsonify({"message": "Proses tracking iklan dimulai"}), 200
+    data = request.get_json()
+    start_id = data.get("id", 1) 
+    tracker.track_listings(start_id=start_id)
+    return jsonify({"message": f"Proses tracking iklan dimulai dari ID {start_id}"}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5005, debug=True)
